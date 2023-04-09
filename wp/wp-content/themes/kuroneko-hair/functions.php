111da5e5db1c441ac605897f8c51df400e1b5340
Yusuke_Saito <ysaito8015@gmail.com>
2023-04-09 15:11:13 +0900

    initial neko_enqueue_scripts

--- wp/wp-content/themes/kuroneko-hair/functions.php
+++ wp/wp-content/themes/kuroneko-hair/functions.php
@@ -3,4 +3,9 @@ function neko_theme_setup() {
     add_theme_support( 'title-tag' );
 }
 add_action( 'after_setup_theme', 'neko_theme_setup' );
+
+function neko_enqueue_scripts(){
+
+}
+add_action( 'wp_enqueue_scripts', 'neko_enqueue_scripts' );
 ?>
