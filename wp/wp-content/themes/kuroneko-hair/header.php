5d2fff640a5900a10ea08217bd3dc58081a65263
Yusuke_Saito <ysaito8015@gmail.com>
2023-04-09 11:39:56 +0900

    add wp_body_open

--- wp/wp-content/themes/kuroneko-hair/header.php
+++ wp/wp-content/themes/kuroneko-hair/header.php
@@ -14,6 +14,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
     <div class="content-Wrap">
         <header role="banner" class="header">
             <h1 class="header-SiteName">
