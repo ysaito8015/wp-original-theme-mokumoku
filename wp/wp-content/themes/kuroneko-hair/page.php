dfd4ee8b8ecd94fd70c3f414b0d2cefd932a9cac
Yusuke_Saito <ysaito8015@gmail.com>
2023-04-09 15:46:54 +0900

    add if and while statements

--- wp/wp-content/themes/kuroneko-hair/page.php
+++ wp/wp-content/themes/kuroneko-hair/page.php
@@ -3,45 +3,14 @@
             <div class="row">
                 <div class="col-lg-8">
                     <main class="main">
+                    <?php if( have_posts() ) : ?>
+                        <?php while( have_posts() ) : the_post(); ?>
                         <article>
-                            <header class="content-Header">
-                                <h1 class="content-Title">
-                                    店舗案内
-                                </h1>
-                            </header>
-                            <div class="content-Body">
-                                <div class="content-EyeCatch">
-                                    <img src="./assets/img/shop.jpg" alt="">
-                                </div>
-                                <table class="table-striped">
-                                    <tbody>
-                                        <tr>
-                                            <td>住所</td>
-                                            <td>〒000-0000 □□県〇〇市△△区☆☆町000</td>
-                                        </tr>
-                                        <tr>
-                                            <td>電話番号</td>
-                                            <td>000-000-0000</td>
-                                        </tr>
-                                        <tr>
-                                            <td>営業時間</td>
-                                            <td>平日 10:00〜19:00 / 土・日 9:00〜19:00<br>※受付：カットは閉店1時間前まで、カラーまたはパーマは2時間前まで</td>
-                                        </tr>
-                                        <tr>
-                                            <td>休業日</td>
-                                            <td>毎週月曜・第2 &amp; 第4火曜日</td>
-                                        </tr>
-                                        <tr>
-                                            <td>スタッフ</td>
-                                            <td>スタイリスト3名／アシスタント2名</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.039827139448!2d137.73227565048458!3d34.70417539044434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601ade7760d07ed9%3A0xf0a52daac3b91a51!2z5rWc5p2-!5e0!3m2!1sja!2sjp!4v1619326103946!5m2!1sja!2sjp" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-                            </div>
                         </article>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
                     </main>
-                </div>
+                </div> <!-- / .col-lg-8 -->
                 <div class="col-lg-4">
                     <aside class="sidebar">
                         <div class="widget_block">
@@ -63,7 +32,7 @@
                             </ul>
                         </div>
                     </aside>
-                </div>
-            </div>
-        </div>
+                </div> <!-- / .col-lg-4 -->
+            </div> <!-- / .row -->
+        </div> <!-- / .container-fluid content -->
 <?php get_footer(); ?>
