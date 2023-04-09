5779ccb3f7e685115d256102a3a49a7254d6dd67
Yusuke_Saito <ysaito8015@gmail.com>
2023-04-09 10:57:36 +0900

    update charset in header

--- wp/wp-content/themes/kuroneko-hair/header.php
+++ wp/wp-content/themes/kuroneko-hair/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="UTF-8">
+    <meta <?php bloginfo( 'charset' ); ?>>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="ゆったり時間と癒しの美容室サンプルサイト">
