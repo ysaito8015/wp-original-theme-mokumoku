4832d38288f6d45f84529024675e5f6df2232b43
Yusuke Saito <info@ysaito.dev>
2025-05-06 20:27:34 +0900

    Done ch05.step04

---
+++ wp/wp-content/themes/kuroneko-hair/template-parts/parts-pagination.php
@@ -0,0 +1,8 @@
+<?php
+    the_posts_pagination(
+        array(
+            'prev_text' => '&lt;<span class="sr-only">前</span>',
+            'next_text' => '&gt;<span class="sr-only">次</span>',
+        )
+    );
+?>
